42246ccca9e4314e188705b8cb844887a80088c3
Korey Pace <korey.pace@gmail.com>
2016-12-14 22:13:30 -0500

    added aws

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import express from 'express';
+import cors from 'cors';
 
 ReactDOM.render(
   <App />,
